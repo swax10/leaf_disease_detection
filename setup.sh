163fcd4a6c19fd4720dc752b9c0a4d5f14936208
swax <swax@swax.com>
2024-03-29 13:29:22 +0530

    fixed xname error

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ echo "Building Docker Image..."
 docker build -t leaf-disease-detection /root/leaf_disease_detection
 
 
-echo "Spinning up Docker Container..."
+echo "Spinning up Docker Container."
 
-docker run -d -p 80:5000 --name server1 leaf-disease-detection
+docker run -d -p 80:5000 --name "$cname" leaf-disease-detection
 
 docker exec -it "$cname" /bin/bash
